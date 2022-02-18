082e1261e53317c549dcc0c166742f351782f72b
zeeshanyousaff <99469177+zeeshanyousaff@users.noreply.github.com>
2022-02-18 15:46:37 +0500

    Delete RestAssured directory

--- RestAssured/src/test/java/RestAssured/git_Develop.java
+++
@@ -1,9 +0,0 @@
-package RestAssured;
-
-public class git_Develop {
-
-    public static void main  (String[] args)
-    {
-        System.out.println("this test case is for git develop branch");
-    }
-}
