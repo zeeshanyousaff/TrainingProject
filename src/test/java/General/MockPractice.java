4fd8c048aa55dc8da333e90551144f8fb54d0472
zeeshanyousaff <zeeshanyousaff@hotmail.com>
2022-02-22 17:40:39 +0500

    updated createuser method

---
+++ src/test/java/General/MockPractice.java
@@ -0,0 +1,31 @@
+package General;
+
+public class MockPractice {
+
+   /*
+    public static void main(String[] args)
+    {
+        //count of upper and lower letters in a string
+        String s="Welcome To Nisum";
+        int upper=0;
+        int lower=0;
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                upper++;
+            } else if (ch >= 'a' && ch <= 'z') {
+                lower++;
+            }
+        }
+        System.out.println("Upper count: " + upper);
+        System.out.println("Lower count: " + lower);
+    }
+    */
+
+    public static void main(String[] args){
+
+
+    }
+
+}
--- src/test/java/RestAssured/git_Develop.java
+++ src/test/java/RestAssured/git_Develop.java
@@ -5,5 +5,6 @@ public class git_Develop {
     public static void main  (String[] args)
     {
         System.out.println("this test case is for git develop branch");
+        
     }
 }
